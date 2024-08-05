f13a3455ed650c550d3ab43a6f0a990816d21518
Mille Elfver <mille.elfver98@gmail.com>
2024-08-05 12:42:23 +0200

    Refactor MainWindow and add SimpleMath class
    Replaced `dotButton` with `commaButton` in `MainWindow.xaml` and added `commaButton_Click` event handler. Introduced `selectedOperator` field and updated `EqualsButton_Click` and `OperationButton_Click` methods to handle arithmetic operations based on `selectedOperator`. Refactored `NumberButton_Click` method for better number selection. Added `SelectedOperator` enum and `SimpleMath` class with static methods for basic arithmetic operations.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         double lastNumber, result;
+        SelectedOperator selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,7 +30,27 @@ namespace Calculator
 
         private void EqualsButton_Click(object sender, RoutedEventArgs e)
         {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = SimpleMath.Subtraction(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                }
 
+                resultLabel.Content = result.ToString();
+            }
         }
 
         private void PercentageButton_Click(object sender, RoutedEventArgs e)
@@ -61,16 +82,99 @@ namespace Calculator
             {
                 resultLabel.Content = "0";
             }
+
+            if (sender == multiplicationButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divideButton)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == plusButton)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusButton)
+                selectedOperator = SelectedOperator.Subtraction;
+        }
+
+        private void commaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains(","))
+            {
+                //do nothing
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content},";
+            }
         }
 
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            int selectedValue = int.Parse((sender as Button).Content.ToString());
+            int selectedValue = 0;
+
+            if (sender == zeroButton)
+                selectedValue = 0;
+            if (sender == oneButton)
+                selectedValue = 1;
+            if (sender == twoButton)
+                selectedValue = 2;
+            if (sender == threeButton)
+                selectedValue = 3;
+            if (sender == fourButton)
+                selectedValue = 4;
+            if (sender == fiveButton)
+                selectedValue = 5;
+            if (sender == sixButton)
+                selectedValue = 6;
+            if (sender == sevenButton)
+                selectedValue = 7;
+            if (sender == eightButton)
+                selectedValue = 8;
+            if (sender == nineButton)
+                selectedValue = 9;
 
             if (resultLabel.Content.ToString() == "0")
-                resultLabel.Content = selectedValue.ToString();
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
             else
+            {
                 resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+        //private void NumberButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    int selectedValue = int.Parse((sender as Button).Content.ToString());
+
+        //    if (resultLabel.Content.ToString() == "0")
+        //        resultLabel.Content = selectedValue.ToString();
+        //    else
+        //        resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+        //}
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtraction(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Divide(double n1, double n2)
+        {
+            return n1 / n2;
         }
     }
 }
